b43dc3a0ac0b81435a4b85b9ec3a632350d8db25
hammar <hammar@deloitte.tn>
2023-11-27 18:38:36 +0100

    add types for the Third-Party API service

--- services/cities.ts
+++ services/cities.ts
@@ -1,8 +1,9 @@
 import fetch from 'node-fetch';
+import { City } from '../types/cities';
 
 
 
-export const getCitiesFromAPI = async (zipcode : string) : Promise<any> => {
+export const getCitiesFromAPI = async (zipcode : string) : Promise<[City]> => {
 
    const url = process.env.GEO_GOV_URL + `?codePostal=${zipcode}`;
    
---
+++ types/cities.ts
@@ -0,0 +1,10 @@
+export type City = {
+    nom: string;
+    code: string;
+    codeDepartement: string;
+    siren: string;
+    codeEpci: string;
+    codeRegion: string;
+    codesPostaux: string[];
+    population: number;
+  };
